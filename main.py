5db7aeeabca7c92f740101be7802e4c73461fe2c
nomenaharivelo <76721261+nomenaharivelo@users.noreply.github.com>
2020-12-29 08:57:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import pygame, sys
 
+pygame.init()
+
+size = (500, 500)
+
+screen = pygame.display.set_mode(size)
+
+while 1:
+  for evt in pygame.event.get():
+    if evt.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit()
